f94156bef5065aec136065bacae580a9652769e3
濁沼広樹 <nigohiroki@gmail.com>
2023-04-20 11:29:34 +0900

    hot fix

--- tags_generator.py
+++ tags_generator.py
@@ -7,7 +7,7 @@ class TagsGenerator:
 
     def generate_tags(self, text):
         # Prepare the prompt
-        full_prompt = f"Generate tags for the following prompt: {prompt}"
+        full_prompt = f"Generate tags as list for the following prompt: {text}"
         # Send the API request
         response = openai.Completion.create(
             engine="text-davinci-002",
@@ -15,7 +15,7 @@ class TagsGenerator:
             max_tokens=50,
             n=1,
             stop=None,
-            temperature=0.7,
+            temperature=0.2,
         )
         tags_str = response.choices[0].text.strip()
         tags = [tag.strip() for tag in tags_str.split(',')]
