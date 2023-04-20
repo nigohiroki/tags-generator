9e69f0f4b984377ae55070102b9aa148eac2fbe3
濁沼広樹 <nigohiroki@gmail.com>
2023-04-20 18:59:46 +0900

    Add 2 attributes

--- README.md
+++ README.md
@@ -2,9 +2,17 @@
 
 A Python library to generate a tags list from a text using OpenAI.
 
+## Features
+
+- Generate tags list from a text
+- Limit the number of tags (max_tags)
+- Order tags by relevance or randomly (order)
+
 ## Installation
 
 Install the package using pip:
+
+pip install tags-generator
 
 
 ## Usage
@@ -15,8 +23,8 @@ from tags_generator import TagsGenerator
 # Set your OpenAI API key
 openai_api_key = "your_openai_api_key"
 
-# Initialize the TagsGenerator
-tg = TagsGenerator(openai_api_key)
+# Initialize the TagsGenerator with custom attributes
+tg = TagsGenerator(openai_api_key, max_tags=5, order='relevance')
 
 # Generate tags from a text
 text = "Create a step-by-step guide on building a pip library to distribution the library."
@@ -24,3 +32,7 @@ tags = tg.generate_tags(text)
 
 print(tags)
 
+
+## Attributes
+- max_tags (integer, default=5): Define the maximum number of tags in the output list.
+- order (string, default='relevance'): Specify the order of tags based on their relevance. Options are 'relevance' or 'random'.
--- tags_generator/tags_generator.py
+++ tags_generator/tags_generator.py
@@ -1,22 +1,24 @@
 import openai
+import ast
 
 class TagsGenerator:
     def __init__(self, openai_api_key):
         self.api_key   = openai_api_key
         openai.api_key = self.api_key
-
-    def generate_tags(self, text):
+    
+    def generate_tags(self, text, max_tags=5, order="relevance"):
         # Prepare the prompt
-        full_prompt = f"Generate tags as list for the following prompt: {text}"
+        full_prompt = f"Generate a python list of tags for the following text: \"{text}\". Limit the number of tags to {max_tags} and order them by {order}. The output sample is [\"tag1\", \"tag2\", ...]"
         # Send the API request
         response = openai.Completion.create(
-            engine="text-davinci-002",
+            engine="text-davinci-003",
             prompt=full_prompt,
             max_tokens=50,
-            n=1,
-            stop=None,
-            temperature=0.2,
+            top_p=1.0,
+            temperature=0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0,
         )
-        tags_str = response.choices[0].text.strip()
-        tags = [tag.strip() for tag in tags_str.split(',')]
-        return tags
+        tags_str  = response.choices[0].text.strip()
+        tags_list = ast.literal_eval(tags_str)
+        return tags_list
